46fb00d8e2c175e122184adf75fef7f7aab96dcd
Daniel P. Volpato <danielpvolpato@gmail.com>
2019-02-19 10:24:07 -0300

    [bash_aliases] Add several alert aliases

--- bash_aliases.d/001-basic.sh
+++ bash_aliases.d/001-basic.sh
@@ -32,6 +32,11 @@ alias now='date +"%T"'
 alias timestamp='date +%Y%m%d_%H%M%S'
 alias path='echo -e ${PATH//:/\\n}'
 
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias alert-sound='while true; do sleep 1; echo -e -n "\a"; done'
+alias soundalert=alert-sound
+alias alert-done='echo "Done at: $(date)"; alert "Done at: $(date)"; alert-sound'
+
 
 ##
 ## Common apps
