970e745cec74cd1e514f83270eb999f45ef5fb5f
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2023-10-16 16:23:17 -0300

    zsh: add powerline10k theme and plugins

--- shell-zsh.zsh
+++ shell-zsh.zsh
@@ -11,6 +11,28 @@
   exit 1
 }
 
+USER_ZSH_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+echo "[zsh] Install plugins..."
+
+echo "[zsh] Plugin: zsh-autosuggestions"
+rm -rf "${USER_ZSH_PATH}/plugins/zsh-autosuggestions"
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${USER_ZSH_PATH}/plugins/zsh-autosuggestions"
+
+echo "[zsh] Plugin: zsh-syntax-highlighting"
+rm -rf "${USER_ZSH_PATH}/plugins/zsh-syntax-highlighting"
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${USER_ZSH_PATH}/plugins/zsh-syntax-highlighting"
+
+echo "[zsh] Plugin: zsh-tmux-auto-title"
+rm -rf "${USER_ZSH_PATH}/plugins/zsh-tmux-auto-title"
+git clone --depth=1 https://github.com/mbenford/zsh-tmux-auto-title "${USER_ZSH_PATH}/plugins/zsh-tmux-auto-title"
+
+
+echo "[zsh] Install themes..."
+echo "[zsh] Theme: powerlevel10k"
+rm -rf ${USER_ZSH_PATH}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${USER_ZSH_PATH}/themes/powerlevel10k
+
 echo "[zsh] Remove invalid symlinks from target dir"
 pushd "$ZSH_CUSTOM" &> /dev/null
 for f in *.zsh; do
