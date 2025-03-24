472d0999cfbf13969c18c5c0edb189b7cbdcef56
Daniel Pereira Volpato <volpato.swe@gmail.com>
2025-03-24 10:00:27 -0300

    Move all scripts but install.sh to scripts/ folder

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-./apt.sh
+# TODO fail on error
 
-./oh-my-zsh.sh
+./scripts/apt.sh
+
+./scripts/oh-my-zsh.sh
 
 pushd dotfiles || exit
 ./install.sh
 popd || exit &> /dev/null 
 
+pushd scripts || exit
 ./fonts.sh
 
 ./shell-zsh.zsh
 ./shell-bash.sh
 
-./nvm.sh
+#./nvm.sh
 
-./pyenv.sh
+#./pyenv.sh
+popd || exit &> /dev/null
 
