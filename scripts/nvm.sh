16a36e82fd82fd410265664cd3c91db54ac259c9
Daniel Pereira Volpato <volpato.swe@gmail.com>
2025-06-24 14:01:39 -0300

    [nvm] update to v0.40.3

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "[nvm] Downloading and executing installer..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # Add to path so we can install latest LTS
 export NVM_DIR="$HOME/.nvm"
